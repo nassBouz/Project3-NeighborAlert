0beb0baf955ce5e357907a459b114af2088847ff
nassBouz <bouzianenassima@gmail.com>
2019-03-18 16:47:24 -0700

    set up files structure

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, g
+from flask import render_template, flash, redirect, url_for
+import json
+
+
+DEBUG = True
+PORT = 8000
+
+app = Flask(__name__)
+app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
+
+@app.route('/')
+def index():
+    return 'hello'
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=PORT)
