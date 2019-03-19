b8e6d2124ec4337448acb5b1fb891b8e15776db6
alom310 <alom310@gmail.com>
2019-03-19 16:51:16 -0700

    login function

--- app.py
+++ app.py
@@ -1,17 +1,81 @@
 from flask import Flask, g
 from flask import render_template, flash, redirect, url_for
-import json
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
+
+import models
+import forms
 
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
-app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
+app.secret_key= 'NeighborAlertsecretword'
+
+login_manager = LoginManager()
+##sets up our login for the app
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
 
 @app.route('/')
 def index():
     return render_template('layout.html')
-    
+
+@app.route('/signup', methods=('GET', 'POST'))
+def signup():
+    form = forms.SignUpForm()
+    if form.validate_on_submit():
+        flash('Yay you registered', 'success')
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+            fullname=form.fullname.data
+            )
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+@app.route('/signin', methods=('GET', 'POST'))
+def signin():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("your email or password doesn't match", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                ## creates session
+                login_user(user)
+                flash("You've been logged in", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("your email or password doesn't match", "error")
+    return render_template('login.html', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
