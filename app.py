ba1c30255a482bf61e9c0371565a3ec0a1bb1c28
alom310 <alom310@gmail.com>
2019-03-21 14:22:56 -0700

    post templates

--- app.py
+++ app.py
@@ -88,11 +88,11 @@ def neighborpage(neighborid):
     if form.validate_on_submit():
         models.Post.create(
             user=g.user._get_current_object(),
-            title=form.title(), 
-            text=form.text(),
-            address=form.address(),
-            imgUrl=form.imgUrl(),
-            category=form.category(),
+            title=form.title.data, 
+            text=form.text.data,
+            address=form.address.data,
+            imgUrl=form.imgUrl.data,
+            category=form.category.data,
             neighbor=neighbor_model)
         return redirect("/{}".format(neighborid))
 
@@ -103,7 +103,30 @@ def profilepage(username):
     user = models.User.get(models.User.username == username)
     return render_template('user.html', user=user) 
 
-    
+@app.route('/posts')
+@app.route('/posts/<id>', methods =['GET','POST'])
+def posts(id=None):
+      if id == None:
+        posts = models.Post.select().limit(100)
+        return render_template('posts.html', posts=posts)
+      else:
+        post_id = int(id)
+        post = models.Post.get(models.Post.id == post_id)
+        comments = post.comments
+
+        form = CommentForm()
+        if form.validate_on_submit():
+            models.Comment.create(
+                user= form.user.data.strip(),
+                text= form.text.data.strip(),
+                post=post
+            )
+
+            return redirect("/posts/{}".format(post_id))
+
+
+        return render_template('post.html', post=post, form=form, comments=comments)
+
 
 @app.route('/logout')
 @login_required
